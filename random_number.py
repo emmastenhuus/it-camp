f3ff68257bcbba8a42ddf0e8487a8d75f255cc07
Emma Stenhuus <emma@stenhuus.dk>
2022-10-23 12:41:00 +0200

    Improved random number game

--- random_number.py
+++ random_number.py
@@ -3,15 +3,21 @@ from random import randint
 number = randint(1, 100)
 guess = 0
 
-while guess != number:
-    number_of_guesses = int(input("How many guesses do you want? "))
-    for i in range(number_of_guesses):
-        guess = int(input("Guess a number between 1 and 100: "))
-        if guess == number:
-            print(f"That's correct! You guessed {i} times.")
-        elif guess < number:
-            print("That's too small.")
-        else:
-            print("That's too big.")
-    break
-print("You're an idiot!")
+found_it = False
+max_guesses = int(input("How many guesses do you want? "))
+attempts = 0
+
+while attempts < max_guesses and guess != number:
+    attempts += 1
+    guess = int(input("Guess a number between 1 and 100: "))
+    if guess == number:
+        found_it = True
+    elif guess < number:
+        print("That's too small.")
+    else:
+        print("That's too big.")
+
+if found_it:
+    print(f"That's correct! You guessed {attempts} times.")
+else:
+    print("You're an idiot!")
