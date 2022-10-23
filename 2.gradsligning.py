c6204dccc9009b8345c1172327662a4a1b2592dd
Mads Stenhuus <mads.stenhuus@hpe.com>
2022-10-23 14:06:59 +0200

    Fixed bug in quadratric equation root solver

--- 2.gradsligning.py
+++ 2.gradsligning.py
@@ -7,11 +7,11 @@ c = int(input("Vælg c-værdi: "))
 d = b * b - 4 * a * c
 
 if d > 0:
-    x1 = (-b + sqrt(d)) / 2 * a
-    x2 = (-b - sqrt(d)) / 2 * a
+    x1 = (-b + sqrt(d)) / (2 * a)
+    x2 = (-b - sqrt(d)) / (2 * a)
     print(f"Svaret er {x1} og {x2}")
 elif d == 0:
-    x1 = (-b) / 2 * a
+    x1 = (-b) / (2 * a)
     print(f"Svaret er {x1}")
 else:
     print("Der er intet svar")
