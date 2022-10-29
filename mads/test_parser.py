1ebf9d6f4fd499aa69a23ea55f5a1c991a6ae616
Mads Stenhuus <mads.stenhuus@hpe.com>
2022-10-29 11:33:24 +0200

    Added math expression parser

---
+++ mads/test_parser.py
@@ -0,0 +1,26 @@
+import unittest
+from util.parser import to_infix_str, to_rpn_str
+
+class Test(unittest.TestCase):
+
+    def test_infix_str(self):
+        self.assertEqual("a + b", to_infix_str("a+b"))
+        self.assertEqual("a!", to_infix_str("a  !"))
+        self.assertEqual("sin(x) ^ b", to_infix_str("sin (  x)^  b"))
+
+    def test_reverse_polish_notation_str1(self):
+        self.assertEqual("x 2 ^", to_rpn_str("x^2"))
+        self.assertEqual("x a sin ^", to_rpn_str("x^sin(a)"))
+        self.assertEqual("b fact", to_rpn_str("b !"))
+        self.assertEqual("b fact", to_rpn_str("fact(b)")) # "fact()" function and "!" operator are synonymous
+
+    def test_reverse_polish_notation_str2(self):
+        self.assertEqual("b fact sin", to_rpn_str("sin(fact(b))"))
+        self.assertEqual("b fact sin", to_rpn_str("sin(b!)"))
+        # self.assertEqual("b sin fact", to_rpn_str("sin(b)!")) 
+        # self.assertEqual("x cos sin fact", to_rpn_str("sin(cos(x))!")) 
+        # self.assertEqual("x cos fact sin", to_rpn_str("sin(cos(x)!)")) 
+        # self.assertEqual("a x sin fact 3 * -", to_rpn_str("a-sin(x)!*3")) # TODO -- "fact" is fishy 
+
+if __name__ == "__main__":
+    unittest.main()
