865a918d909f90ae618ceed2da130494ca1f314d
Mads Stenhuus <mads@stenhuus.dk>
2022-10-29 11:52:45 +0200

    Minor change

--- mads/cls_test.py
+++ mads/cls_test.py
@@ -1,25 +1,26 @@
 
 class Tst:
-    y = 0
-
     def __init__(self, x) -> None:
         self.x = x
 
     @classmethod
     def i(cls, z):
-        cls.y = z
+        Tst.y = z
 
     def __str__(self) -> str:
         return str(self.x) + " / " + str(Tst.y)
 
+    def __repr__(self) -> str:
+        return str()
+
 t1 = Tst(11)
 t2 = Tst(13)
-Tst.i(8)
+t1.i(8)
 
 print(t1)
 print(t2)
 
-Tst.i(3)
+t2.i(3)
 
 print(t1)
 print(t2)
